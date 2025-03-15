05a01f998ea8f2115b9ef8e8b1fcee1af2d2572a
VyasNml <142661775+VyasNml@users.noreply.github.com>
2025-03-15 20:33:52 +0530

    Add files via upload

---
+++ LC_1768_E2_Alternate_String.py
@@ -0,0 +1,55 @@
+'''
+1768. Merge Strings Alternately
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+ If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+'''
+
+L1 = input()
+L2 = input()
+L3 = ""
+
+while True:
+    L3 += L1[0]
+    L3 += L2[0]
+    
+    L1 = L1[1:len(L1)]
+    L2 = L2[1:len(L2)]
+
+    if L1=="" or L2=="":
+        break
+
+print(L3)
