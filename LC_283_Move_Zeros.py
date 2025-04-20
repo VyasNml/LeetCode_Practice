68c7a1dbf52d6ec59bb6da179bdf79fa5dc1677e
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2025-04-20 22:31:58 +0530

    Update LC_283_Move_Zeros.py

--- LC_283_Move_Zeros.py
+++ LC_283_Move_Zeros.py
@@ -22,11 +22,7 @@ Constraints:
 '''
 
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        
+    def moveZeroes(self, nums: List[int]) -> None:       
         j=0
         for i in range(len(nums)):
             if nums[i]!=0:
