ac253b7fd3a73560fecefe4d9b9a5d1621c07b91
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2025-07-07 14:23:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (67.11%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(isdigit(s[i])){
-                st.pop();
-                continue;
+        int i=0;
+        while(true){
+            if(i>=s.size())break;
+            if(isdigit(s[i+1])){
+                s.erase(i,2);
+                if(i!=0){
+                    i--;
+                }  
+                    
+            }else{
+                i++;
             }
-            st.push(s[i]);
-        }
-        s = "";
-        while(!st.empty()){
-            s+=st.top();
-            st.pop();
-        }
-        reverse(s.begin(),s.end());   
+        } 
         return s;
     }
 };
