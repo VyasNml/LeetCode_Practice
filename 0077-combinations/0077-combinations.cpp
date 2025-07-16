914ac9e74330d22d8dfdd3384bb5e845c9d11bff
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2025-07-16 08:37:50 +0530

    Time: 37 ms (65.47%), Space: 62.5 MB (80.17%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    void sub(vector<vector<int>>& nums,vector<int>& arr,int index,int& n,int& k){
+    void sub(vector<vector<int>>& p ,vector<int>& arr,int index,int k,int n){
         if(arr.size()==k){
-            nums.push_back(arr);
+            p.push_back(arr);
+            return;
         }
-        for(int i=index;i<=n;i++){
-            arr.push_back(i);
-            sub(nums,arr,i+1,n,k);
+
+        for(int i=index; i<n; i++){
+            // if(i>n || arr.size()>k)return;
+            
+            arr.push_back(i+1);
+            sub(p,arr,i+1,k,n);
             arr.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> nums;
+        // set<set<int>> p;
         vector<int> arr;
-        sub(nums,arr,1,n,k);
+        sub(nums,arr,0,k,n);
+        // for(auto x:p){
+        //     vector<int> ar1;
+        //     for(int y:x){
+        //         ar1.push_back(y);
+        //     }
+        //     nums.push_back(ar1);
+        //     ar1.clear();
+        // }
         return nums;   
     }
 };
