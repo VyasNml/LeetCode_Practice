4403fe7e262a79689f1949079d7f33e0ef4ff6b0
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2026-01-24 15:02:30 +0530

    Time: 0 ms (100%), Space: 97.4 MB (24.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& nums) {
-        int low=INT_MAX,high=INT_MIN,max=INT_MIN;
+        int min=INT_MAX,max=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<low)low = nums[i];high=0;
-            if(nums[i]>high)high = nums[i];
-            if(max < high-low)max = high-low;
+            min = std::min(min,nums[i]);
+            max = std::max(max,nums[i]-min);
+            // if(nums[i]<min){
+                // min = nums[i];
+            // }
+
+            // if(nums[i]>max)max = std::max(max,max-min);
         }
         return max;
     }
