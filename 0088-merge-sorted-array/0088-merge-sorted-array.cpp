455713cacbcf2da07025b6c91045baff672c9aa1
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2026-01-27 09:29:34 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0,j=0,k=0;
-        vector<int> ans;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                ans.push_back(nums1[i++]);
-            }else{
-                ans.push_back(nums2[j++]);
-            }
+        for(int i=0;i<n;i++){
+            nums1[m++]=nums2[i];
         }
-        if(i==m && j<n){
-            for(int z = j;z<n;z++){
-                ans.push_back(nums2[j++]);
-            }
-        }
-        if(i<m && j==n){
-            for(int z = i;z<m;z++){
-                ans.push_back(nums1[i++]);
-            }
-        }
-        nums1 = ans;
+        sort(nums1.begin(),nums1.end());
     }
 };
