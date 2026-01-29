abb65161286a05bd6eb0748540babb2c7a44ccb8
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2026-01-29 13:52:00 +0530

    Time: 1 ms (55.06%), Space: 9.9 MB (10.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
+
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        if(s.length()!=t.length())return false;
+        unordered_map<char,int> map;
+        for(char x:s){
+            map[x]++;
+        }
+        for(char x:t){
+            map[x]--;
+        }
+        for(auto x:map){
+            if(x.second!=0)return false;
+        }
+        return true;
     }
 };
