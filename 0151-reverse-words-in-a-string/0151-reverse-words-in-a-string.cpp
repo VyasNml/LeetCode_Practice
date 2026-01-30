ca41da834ff16e73d7f42144b974912c77b8cc69
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2026-01-30 12:57:44 +0530

    Time: 0 ms (100%), Space: 10 MB (87.36%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        unordered_map<int,string> words;
-        int i=0,count=0;
-        string w="";
-        while(i<s.length()){//O(n) -> O(n)
-            while(s[i]==' ' && i< s.length()){
-                i++;
-            }//O(1)
-            while(s[i]!=' ' && i<s.length()){
-                w+=s[i++];
-            }//O(word length)
-            if(w!=""){
-                words[count++] = w;
-                w="";
+        int i=0;
+        // if space skip till a letter
+        // if word loop till space
+        // push word
+        vector<string> st;
+        string ans="";
+        while(i<s.size()){
+            if(s[i]==' '){
+                while(s[i]==' ' && i<s.size()){
+                    i++;
+                }
+            }else{
+                string str ="";
+                while(s[i]!=' ' && i<s.size()){
+                    str+=s[i++];
+                }
+                // st.push_back(str);
+                ans.insert(0," "+str);
             }
-            i++;
         }
-        s="";
-        for(int i=count-1;i>=0;i--){
-            s+=words[i];
-            if(i!=0)s+=" ";
-        } //O(number of words)
-        return s;  //---> O(n) + O(words)
+        ans.erase(0,1);
+        return ans;
     }
 };
