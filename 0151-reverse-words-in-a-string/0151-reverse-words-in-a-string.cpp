92edc56d97c77a8ec0bf0976cc834781a05293d6
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2026-02-28 23:20:25 +0530

    Time: 3 ms (35.83%), Space: 10 MB (79.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -5,7 +5,6 @@ public:
         // if space skip till a letter
         // if word loop till space
         // push word
-        vector<string> st;
         string ans="";
         while(i<s.size()){
             if(s[i]==' '){
@@ -17,7 +16,6 @@ public:
                 while(s[i]!=' ' && i<s.size()){
                     str+=s[i++];
                 }
-                // st.push_back(str);
                 ans.insert(0," "+str);
             }
         }
