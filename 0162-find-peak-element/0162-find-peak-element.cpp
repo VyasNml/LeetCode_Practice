7bce44f4cb74bcfc55e353eb794f7197c558146d
Vyas Nademmal <142661775+VyasNml@users.noreply.github.com>
2026-06-26 11:57:24 +0530

    Time: 0 ms (100%), Space: 12.5 MB (71.71%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,10 +5,11 @@ public:
         if(r==0)return 0;
         while(l<r){
             int m = (l+r)/2;
+            //if mid is less than its next ele, peak must be in the right half
             if(nums[m]<nums[m+1]){
                 l = m+1;
             }else{
-                r = m; 
+                r = m; //not m-1
             }
         }
         return l; 
