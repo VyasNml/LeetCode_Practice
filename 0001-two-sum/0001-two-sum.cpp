a81f4c144ae21d9e29428cad8389689b43468344
Vyas Nademmal <vyasnml@gmail.com>
2026-08-18 23:43:37 +0530

    Time: 108 ms (5.22%), Space: 14.1 MB (73.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
                 if(nums[j]+nums[i]==target){
-                    nums.clear();
-                    nums.push_back(i);
-                    nums.push_back(j);
+                    ans.push_back(i);
+                    ans.push_back(j);
                     break;
                 }
             }
         }
-        return nums;
+        return ans;
     }
 };
