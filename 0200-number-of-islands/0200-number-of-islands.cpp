6cc09fba31260c713821286e85f69f2018df75ef
Vyas Nademmal <vyasnml@gmail.com>
2026-09-18 23:57:17 +0530

    Time: 21 ms (91.66%), Space: 16.3 MB (69.75%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    void DFS(int r, int c,vector<vector<char>>& image){
-
-        if(r<0 || r>=image.size() || c<0 || c>=image[0].size() || image[r][c]=='0')return;
-        image[r][c] = '0';
-        
-        // top
-        DFS(r-1,c,image);
-        // bottom
-        DFS(r+1,c,image);
-        // left
-        DFS(r,c-1,image);
-        // right
-        DFS(r,c+1,image);
+    void DFS(vector<vector<char>>& grid, int& r, int& c, int i,int j){
+        if(i<0 || i>=r || j<0 || j>=c)return;
+        if(grid[i][j]=='1'){
+            grid[i][j]='0';
+            DFS(grid,r,c,i+1,j);
+            DFS(grid,r,c,i-1,j);
+            DFS(grid,r,c,i,j+1);
+            DFS(grid,r,c,i,j-1);
+        }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
         int count = 0;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int r = grid.size();
+        int c = grid[0].size();
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
+                    DFS(grid,r,c,i,j);
                     count++;
-                    DFS(i,j,grid);
                 }
             }
         }
         return count;
     }
-    
 };
